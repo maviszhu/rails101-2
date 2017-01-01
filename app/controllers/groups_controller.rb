bd03b6246636daf255504a87c22cabc7542400a5
maviszhu <maviszhu1987@gmail.com>
2017-01-01 16:08:25 +0800

    create groups index

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,16 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+#def edit
+#end
+  def create
+  end
+  def show
+  end
+  def update
+  end
+  # def destroy
+  # end
+
+end
